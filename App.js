bc658ce5509c38693fad9f700fd14751eba160b5
Wizzycool0001 <wisdomdanobu57@gmail.com>
2024-02-16 14:50:20 +0100

    new changes

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
+
 const app = () => {
   return (
     <NavigationContainer>
